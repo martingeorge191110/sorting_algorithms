e92cee1422f7c1935e985fe755c169eecf95f93b
Martin George <martingeorge191110@gmail.com>
2024-10-28 12:44:20 +0300

    Adjusting quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -51,7 +51,7 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low;
 	int j;
 
 	for (j = low; j < high; j++)

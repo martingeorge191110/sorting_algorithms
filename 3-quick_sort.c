1197c9446e72446d55759ef4c859c93c7654898b
M-sharkawy <mostafaeelsharkawy@gmail.com>
2024-10-28 09:16:47 +0300

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,22 +1,39 @@
 #include "sort.h"
 
 /**
+ * swap_func - swapping of two variable values
+ *
+ * @a: pointer to first variable
+ * @b: pointer to second variable
+ */
+
+void swap_func(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * quick_sort_helper - recursive helper function for quick sort
  *
  * @array: pointer to the array
  * @low: starting index of the array
  * @high: ending index of the array
+ * @size: array size
  *
  * Return: Nothing
 */
-void quick_sort_helper(int *array, int low, int high)
+void quick_sort_helper(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int pi = lomuto_partition(array, low, high);
+		int pi = lomuto_partition(array, low, high, size);
 
-		quick_sort_helper(array, low, pi - 1);
-		quick_sort_helper(array, pi + 1, high);
+		quick_sort_helper(array, low, pi - 1, size);
+		quick_sort_helper(array, pi + 1, high, size);
 	}
 }
 
@@ -26,24 +43,28 @@ void quick_sort_helper(int *array, int low, int high)
  * @array: pointer to the array
  * @low: starting index of the partition
  * @high: ending index (pivot)
+ * @size: array size
  *
  * Return: the partition index
 */
 
-int lomuto_partition(int *array, int low, int high)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1, j = low;
+	int i = low - 1;
+	int j;
 
-	for (j; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
 			i++;
 			swap_func(&array[i], &array[j]);
+			print_array(array, size);
 		}
 	}
 	swap_func(&array[i + 1], &array[high]);
+	print_array(array, size);
 	return (i + 1);
 }
 
@@ -61,5 +82,5 @@ void quick_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	quick_sort_helper(array, 0, size - 1);
+	quick_sort_helper(array, 0, size - 1, size);
 }
